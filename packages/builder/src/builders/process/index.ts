2ec12d8338e8086f8fcda538fbee385a01903bf7
Chihab Otmani <chihab@gmail.com>
2022-06-16 06:46:06 +0100

    feat: add noBuild option

--- packages/builder/src/builders/process/index.ts
+++ packages/builder/src/builders/process/index.ts
@@ -4,41 +4,50 @@ import {
   BuilderOutput,
 } from "@angular-devkit/architect";
 import { JsonObject } from "@angular-devkit/core";
-import { Project, StructureKind } from "ts-morph";
+import { Project } from "ts-morph";
 import * as ts from "typescript";
+import * as path from "path";
 import { getClientEnvironment } from "../../utils/env";
 
 interface Options extends JsonObject {
   files: string[];
+  outputPath: string;
 }
 
-const { raw, stringified } = getClientEnvironment(/^NG_APP/i);
+const { raw } = getClientEnvironment(/^NG_APP/i);
 
-async function processBuilder(
-  { files }: Options,
-  context: BuilderContext
-): Promise<BuilderOutput> {
+async function processBuilder({
+  files,
+  outputPath,
+}: Options): Promise<BuilderOutput> {
   const [file] = files;
+  console.log({ outputPath });
   try {
-    // initialize
-    const project = new Project();
-    // add source files
-    const source = project.addSourceFileAtPath(files[0]);
-    source.transform((traversal) => {
-      const node = traversal.visitChildren();
-      if (ts.isPropertyAccessExpression(node)) {
-        const processEnv = node.getChildren();
-        if (
-          ts.isPropertyAccessExpression(processEnv[0]) &&
-          processEnv[0].getText() === "process.env" &&
-          ts.isIdentifier(processEnv[2])
-        ) {
-          return ts.factory.createStringLiteral(raw[processEnv[2].getText()]);
+    const sourceProject = new Project();
+    const destinationProject = new Project();
+    const sources = sourceProject.addSourceFilesAtPaths(files);
+    sources.forEach((source) => {
+      console.log(path.resolve(outputPath, source.getBaseName()));
+      const destinationSource = destinationProject.createSourceFile(
+        path.resolve(outputPath, source.getBaseName()),
+        source.getText()
+      );
+      destinationSource.transform((traversal) => {
+        const node = traversal.visitChildren();
+        if (ts.isPropertyAccessExpression(node)) {
+          const processEnv = node.getChildren();
+          if (
+            ts.isPropertyAccessExpression(processEnv[0]) &&
+            processEnv[0].getText() === "process.env" &&
+            ts.isIdentifier(processEnv[2])
+          ) {
+            return ts.factory.createStringLiteral(raw[processEnv[2].getText()]);
+          }
         }
-      }
-      return node;
+        return node;
+      });
     });
-    await project.save();
+    await destinationProject.save();
   } catch (err) {
     return {
       success: false,
